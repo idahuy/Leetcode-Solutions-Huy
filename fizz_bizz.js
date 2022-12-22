d2defbacd02fbd0c236395b55815346c3f8735ac
Huy <drac0123451@gmail.com>
2022-12-21 22:47:30 -0800

    fizz bizz

---
+++ fizz_bizz.js
@@ -0,0 +1,21 @@
+var fizzBuzz = function(n) {
+    let result = [];
+    
+    for(let i = 1; i <= n; i++){
+        if(i % 5 === 0 && i % 3 === 0){
+            result.push("FizzBuzz");
+            continue;
+        }
+        if(i % 3 === 0){
+            result.push("Fizz");
+            continue;
+        }
+        if(i % 5 === 0){
+            result.push("Buzz");
+            continue;
+        }
+        result.push(i.toString());
+    }
+        
+    return result;
+};
